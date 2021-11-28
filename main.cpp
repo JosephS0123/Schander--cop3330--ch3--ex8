7b986669ea8dee13dc2b51de7ef01754ef825458
JosephS0123 <90111749+JosephS0123@users.noreply.github.com>
2021-11-28 16:44:33 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+  int a;
+  scanf("%d",&a);
+  if(a%2==0){
+    printf("the value %d is an even number",a);
+  }
+  else
+  printf("the value %d is an odd number",a);
+} 
